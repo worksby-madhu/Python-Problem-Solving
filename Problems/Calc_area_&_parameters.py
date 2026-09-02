4c70fef83e83f0247adf97898fbb034956c82702
Madhu Lakshmi Subhadra VILLA <lakshmisubhadravilla@gmail.com>
2026-09-02 09:44:12 +0530

    Add rectangle area program

---
+++ Problems/Calc_area_&_parameters.py
@@ -0,0 +1,7 @@
+len=int( input("Enter length:"))
+bth=int(input("Enter breadth:"))
+
+area=len*bth
+parameter=2*len+bth
+
+print("The area of the rectangle is", area, "with length", len, "and breadth", bth)
