6f10f878661000786eb66331cea68de789e4587c
Madhu Lakshmi Subhadra VILLA <lakshmisubhadravilla@gmail.com>
2026-09-02 10:33:40 +0530

    Add rectangle area and perimeter program

--- Programs/Calc_area_&_parameters.py
+++ Programs/Calc_area_&_parameters.py
@@ -2,6 +2,6 @@ len=int( input("Enter length:"))
 bth=int(input("Enter breadth:"))
 
 area=len*bth
-parameter=2*len+bth
+parameter=2*(len+bth)
 
-print("The area of the rectangle is", area, "with length", len, "and breadth", bth)
+print("The area of the rectangle is", area, "and perimeter is", parameter)
