b14c3184f9460b6795070c0b5a36c4619f305d3b
Madhu Lakshmi Subhadra VILLA <lakshmisubhadravilla@gmail.com>
2026-09-13 19:23:28 +0530

    Remove Programs folder

--- Programs/Calc_area_&_parameters.py
+++
@@ -1,7 +0,0 @@
-len=int( input("Enter length:"))
-bth=int(input("Enter breadth:"))
-
-area=len*bth
-parameter=2*(len+bth)
-
-print("The area of the rectangle is", area, "and perimeter is", parameter)
